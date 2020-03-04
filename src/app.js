de9526670d7869e62f498c0cce9fb66fafde906c
brodave318 <brodave318@gmail.com>
2020-03-04 14:52:23 -0800

    Init commit

---
+++ src/app.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const geocode = require('./utils/geocode');
+const forcast = require('./utils/forecast');
+
+// Define paths for express config
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup
+app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Web Dave'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Web Dave'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help page',
+    name: 'Web Dave',
+    message: 'Welcome to the help page'
+  });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address'
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+    forcast(latitude, longitude, (error, forcastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({
+        forecast: forcastData,
+        location,
+        address: req.query.address
+      });
+    });
+  });
+});
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term.'
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: []
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Web Dave',
+    errorMessage: 'Help article not found'
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Web Dave',
+    errorMessage: "Page not found"
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (lati, longi, callback) => {
+  const url = `https://api.darksky.net/forecast/ac410a99faf819eb948cb4118dbe4aed/${lati},${longi}`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to loacation services', undefined);
+    } else if (body.error) {
+      callback('Unable to find coordnates. Try another search', undefined);
+    } else {
+      callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. This is a ${body.currently.precipProbability}% chance of rain.`);
+    };
+  });
+};
+
+module.exports = forecast;
